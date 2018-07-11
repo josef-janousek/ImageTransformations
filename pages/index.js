e2ea8c82ddd92c490030fcd3fb02d1d7a9e4ff5a
Josef Janousek <pauto@centrum.cz>
2018-07-11 21:59:44 +0200

    Adding new operations

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 import SimpleTable from '../components/SimpleTable';
@@ -12,22 +14,102 @@ const styles = theme => ({
     textAlign: 'center',
     paddingTop: theme.spacing.unit,
   },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 190,
+  },
+  menu: {
+    width: 190,
+  },
+  button: {
+    marginBottom: theme.spacing.unit,
+  },
 });
 
 class Index extends React.Component {
   state = {
-    operations: [{id: 0, name: "resize", parameter: 128}, {id: 1, name: "quality", parameter: 60}],
+    operations: [],
+    newOpName: "resize",
+    newOpParameter: 128,
+  };
+
+  definedOps = [{id: 0, name: "resize"}, {id: 1, name: "quality"}];
+
+  addOperation = () => {
+    var numberParam = Number(this.state.newOpParameter);
+    if (isNaN(numberParam) || numberParam <= 0) {
+      alert("Operation parameter must be a positive number.");
+      return;
+    }
+    this.state.operations.push({
+      id: this.state.operations.length,
+      name: this.state.newOpName,
+      parameter: numberParam,
+    });
+    this.setState({
+      operations: this.state.operations,
+    });
+  };
+
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value,
+    });
   };
 
   render() {
     const { classes } = this.props;
-    const { operations } = this.state;
+    const { operations, newOpName, newOpParameter } = this.state;
 
     return (
       <div className={classes.root}>
         <Typography variant="display1" gutterBottom>
           Image transformations
         </Typography>
+
+        <Typography variant="subheading" gutterBottom>
+          New transformation:
+        </Typography>
+
+        <TextField
+          id="select-operation"
+          select
+          label="Operation type:"
+          className={classes.textField}
+          value={newOpName}
+          onChange={this.handleChange('newOpName')}
+          SelectProps={{
+            MenuProps: {
+              className: classes.menu,
+            },
+          }}
+          margin="normal"
+        >
+          {this.definedOps.map(option => (
+            <MenuItem key={option.name} value={option.name}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <TextField
+          id="parameter"
+          required
+          label="Parameter (number):"
+          value={newOpParameter}
+          onChange={this.handleChange('newOpParameter')}
+          type="number"
+          className={classes.textField}
+          InputLabelProps={{
+            shrink: true,
+          }}
+          margin="normal"
+        />
+
+        <Button variant="contained" color="primary" onClick={this.addOperation} className={classes.button}>
+          Add
+        </Button>
 
         <Typography variant="subheading" gutterBottom>
           Transformations pipeline:
